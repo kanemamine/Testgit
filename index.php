6288c87850442beb21c0f68049896917c4ae953c
Amine Kanem <aminekanem9@gmail.com>
2023-10-11 09:53:21 -0400

    code

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
-ierineinfineifbn
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method ="post" action="" >
+        <input type="text" name="password">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
+<?php 
+if ($_POST) {
+    $data = $_POST['password'];
+    echo $data;
+}?>
